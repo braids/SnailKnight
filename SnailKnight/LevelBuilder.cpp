68f37de7a1da39ce7ca098b413b8c10d348dade6
braids <cymoro@gmail.com>
2018-01-13 02:55:59 -0800

    GenerateStaticCurve(), GetBuildPoints(), SetBuildPoints()
    - Created LevelBuilder::GetBuildPoints(), LevelBuilder::SetBuildPoints() funcs to get and set a vector list of points for BuildPosition objects. Used for scaling/transposing curves in GenerateStaticCurves().
    - Added includes for vector, Box2D.h, GameObject.h in LevelBuilder.cpp, and just vector in LevelBuilder.h.
    - Added LevelBuilder::GenerateStaticCurve() func logic, will spit out a vector of StaticRectangles arranged and aligned in a curve to a specified size, thickness, arc, and number of segments in a location.

--- SnailKnight/LevelBuilder.cpp
+++ SnailKnight/LevelBuilder.cpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include "Box2D\Box2D.h"
+#include "GameObject.h"
 #include "LevelBuilder.h"
 
 LevelBuilder::LevelBuilder(b2World* _world) {
@@ -11,9 +14,55 @@ std::vector<StaticRectangle*> LevelBuilder::GenerateStatics(float32 _x, float32 
 	return statics;
 }
 
-std::vector<StaticRectangle*> LevelBuilder::GenerateStaticCurve(float32 _x, float32 _y, float32 _begin, float32 _arc, float32 _scalar)
+std::vector<StaticRectangle*> LevelBuilder::GenerateStaticCurve(float32 _x, float32 _y, float32 _thickness, float32 _angleBegin, float32 _angleArc, int _angleSteps, float32 _curveScalarX, float32 _curveScalarY)
 {
-	return std::vector<StaticRectangle*>();
+	// Init static object vector
+	std::vector<StaticRectangle*> statics(_angleSteps);
+	// Init build positions
+	std::vector<BuildPosition> buildPos = this->GetCurve(_angleBegin, _angleArc, _angleSteps);
+	
+	// Scale and transpose build points
+	std::vector<b2Vec2> buildPoints = this->GetBuildPoints(buildPos);
+	this->ScaleCurve(&buildPoints, _curveScalarX, _curveScalarY);
+	this->TransposeCurve(&buildPoints, _x, _y);
+	this->SetBuildPoints(&buildPos, buildPoints);
+	
+	// Get rectangle size
+	float32 pointDistance = b2Distance(buildPoints[0], buildPoints[1]);
+	b2Vec2 size(pointDistance, _thickness);
+
+	// Create static objects
+	std::vector<BuildPosition>::iterator iter;
+	for (iter = buildPos.begin(); iter != buildPos.end(); iter++)
+		statics.push_back(new StaticRectangle(this->world, iter->Point, size, iter->Angle, &Assets::Instance()->images.b100x25));
+
+	// Return vector list of static objects
+	return statics;
+}
+
+std::vector<b2Vec2> LevelBuilder::GetBuildPoints(std::vector<BuildPosition> _buildPositions)
+{
+	// Init buildPoints vector list
+	std::vector<b2Vec2> buildPoints(_buildPositions.size());
+	// Declare BuildPosition iterator
+	std::vector<BuildPosition>::iterator iter;
+
+	// Copy points from BuildPosition vector list to buildPoints vector list
+	for (iter = _buildPositions.begin(); iter != _buildPositions.end(); iter++)
+		buildPoints.push_back(iter->Point);
+	
+	// Return point vector list
+	return buildPoints;
+}
+
+void LevelBuilder::SetBuildPoints(std::vector<BuildPosition>* _buildPosition, std::vector<b2Vec2> _buildPoints) {
+	// Init vector iterators
+	std::vector<BuildPosition>::iterator bPositionIter = _buildPosition->begin();
+	std::vector<b2Vec2>::iterator bPointIter = _buildPoints.begin();
+
+	// Set buildPosition points from buildPoint vector
+	for (; bPositionIter != _buildPosition->end() || bPointIter != _buildPoints.end(); bPositionIter++, bPointIter++)
+		bPositionIter->Point = *bPointIter;
 }
 
 std::vector<BuildPosition> LevelBuilder::GetCurve(float32 _begin, float32 _arc, int _steps)
--- SnailKnight/LevelBuilder.h
+++ SnailKnight/LevelBuilder.h
@@ -2,6 +2,7 @@
 #ifndef _LEVELBUILDER_H_
 #define _LEVELBUILDER_H_
 
+#include <vector>
 #include "GameObject.h"
 #include "Box2D\Box2D.h"
 
@@ -19,9 +20,12 @@ public:
 
 	std::vector<StaticRectangle*> GenerateStatics(float32 _x, float32 _y);
 
-	std::vector<StaticRectangle*> GenerateStaticCurve(float32 _x, float32 _y, float32 _begin, float32 _arc, float32 _scalar);
-	std::vector<BuildPosition> GetCurve(float32 _begin, float32 _arc, int _steps);
+	std::vector<StaticRectangle*> GenerateStaticCurve(float32 _x, float32 _y, float32 _thickness, float32 _angleBegin, float32 _angleArc, int _angleSteps, float32 _curveScalarX, float32 _curveScalarY);
 	
+	std::vector<b2Vec2> GetBuildPoints(std::vector<BuildPosition> _buildPositions);
+	void SetBuildPoints(std::vector<BuildPosition>* _buildPosition, std::vector<b2Vec2> _buildPoints);
+	std::vector<BuildPosition> GetCurve(float32 _begin, float32 _arc, int _steps);
+
 	void ScaleCurve(std::vector<b2Vec2>* _curve, float32 _scalar);
 	void ScaleCurve(std::vector<b2Vec2>* _curve, float32 _scalarX, float32 _scalarY);
 	void TransposeCurve(std::vector<b2Vec2>* _curve, float32 _x, float32 _y);
